9e3d21fe75c09347fc4c151fe04315b6d4ac5763
Krishan Sritharar <ks1619@ic.ac.uk>
2021-04-11 10:21:56 +0100

    Added single and double bracket encoding

---
+++ encoding.py
@@ -0,0 +1,39 @@
+
+import sys
+
+
+def main():
+    if (len(sys.argv) < 1):
+        return
+
+
+    if (sys.argv[1].lower() == "d"):
+        print(get_double_arrow(int(sys.argv[2])))
+    elif (sys.argv[1].lower() == "s"):
+        print(get_single_arrow(int(sys.argv[2])))
+
+
+# Given an integer, returns its << >> encoding
+# eg get_double_arrow(8) = 3, 0
+def get_double_arrow(a):
+    if (a == 0):
+        return 0, 0
+    temp = a
+    x = 0
+    while (temp % 2 == 0):
+        temp /= 2
+        x += 1
+    y = a / (2 ** x)
+    return int(x), int((y-1)/2)
+
+# Given an integer, returns its < > encoding
+# eg get_single_arrow(27) = 2, 3
+def get_single_arrow(a):
+    if (a == 0):
+        return 0, 0
+    return get_double_arrow(a+1)
+
+
+
+if __name__ == '__main__':
+    main()
